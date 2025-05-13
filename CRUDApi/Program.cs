89f41a0801be085173ef8c1f2ddc3c02f289276f
Bax <chernoff.serg3i@yandex.ru>
2025-05-13 23:27:43 +0300

    Add project files.

---
+++ CRUDApi/Program.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.OpenApi.Models;
+
+namespace CRUDApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUDApi", Version = "v1" });
+            });
+
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(swagger =>
+                {
+                    swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "CRUDApi v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
